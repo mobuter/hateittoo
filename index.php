5028d16dcac5fb6f6a0e190edfef4ca60c1f8e79
mobuter <victor.dube@hotmail.com>
2015-04-01 13:45:34 -0400

    adding site

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Hate it Too</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+</head>
+
+<body>
+	<div id="container">
+    	<img id="logo" src="css/img/logo.png" alt="Hate it Too" title="Hate it Too" />
+        
+        <ul>
+        	<li><a href="https://www.facebook.com/hateittoo" target="_blank"><img src="css/img/facebook.png" alt="Facebook" title="Facebook" /></a></li>
+            <li><a href="https://www.youtube.com/channel/UCingJs8O79pW1TkLiigWtCQ" target="_blank"><img src="css/img/youtube.png" alt="Youtube" title="Youtube" /></a></li>
+            <li><a href="https://hateittoo.bandcamp.com/" target="_blank"><img src="css/img/bandcamp.png" alt="Bandcamp" title="Bandcamp" /></a></li>
+            <li><a href="https://instagram.com/hateittoo/" target="_blank"><img src="css/img/instagram.png" alt="Instagram" title="Instagram" /></a></li>
+        </ul>
+    </div>
+</body>
+</html>
